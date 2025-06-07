81f57cff307093c61fdaf4c5ea93a393a5cc9d70
jay <jkariya524@rku.ac.in>
2025-06-07 11:26:16 +0530

    oop

---
+++ OOP/basics.java
@@ -0,0 +1,24 @@
+package OOP;
+
+public class basics {
+    public static void main(String[] args) {
+        
+        detail st1 = new detail();
+        System.out.println("hello Students:  " + st1.name  );
+    }
+}
+
+class detail {
+    int roll_no ;
+    String  name;
+
+    detail(){
+        this(12, "jay");
+    }
+    detail(int roll_no,String name){
+        this.name = name ;
+        this.roll_no = roll_no;
+    }
+    
+    
+}
