2adb77e645be3648cfb4db890287bdfc814dc1fd
jay <jkariya524@rku.ac.in>
2025-06-07 23:41:08 +0530

    static

---
+++ OOP/ONE.java
@@ -0,0 +1,34 @@
+package OOP;
+
+public class ONE {
+
+    static String name = "lucifer ";
+    static int age ;
+
+    public static void main(String[] args){
+
+        detail n1 = new detail();
+        System.out.println(n1.name);
+        System.out.println(name + age);
+        newfile();
+    }
+   public static void newfile(){
+
+        System.out.println("this is second file");
+        ONE obj = new ONE(); // this is the way u can use non-static method in static method   
+        obj.old();
+
+        // can not use this in static method
+    }
+    // static methods call only static method not non static . 
+     void old(){
+        System.out.println("hero ");
+    }
+
+    // this block initiliaze only once when the class is lodead or object created . 
+    static{
+
+        // name="jay";
+        age = 45;
+    }
+}
--- OOP/basics.java
+++ OOP/basics.java
@@ -1,13 +1,15 @@
 package OOP;
 
+import static OOP.ONE.newfile;
+
 public class basics {
     public static void main(String[] args) {
         
         detail st1 = new detail();
         System.out.println("hello Students:  " + st1.name  );
+        newfile();
     }
 }
-
 class detail {
     int roll_no ;
     String  name;
@@ -19,6 +21,5 @@ class detail {
         this.name = name ;
         this.roll_no = roll_no;
     }
-    
     
 }
