c83f2196bb9e44a577b575b7eb34610a4c84c63e
jay <jkariya524@rku.ac.in>
2025-07-06 23:10:55 +0530

    Largest no

---
+++ DSA/Array/Lrgest_no.java
@@ -0,0 +1,25 @@
+package Array;
+
+public class Lrgest_no {
+
+    public static void main (String[] args){
+
+        int arr[] = {12,78,5,99,45,23};
+        Larg_no(arr);
+    }
+
+    public static void  Larg_no(int[] arr){
+
+        
+        int largest = arr[0];
+
+        for(int j = 1 ; j < arr.length - 1  ; j++){
+
+            if(largest < arr[j]){
+                largest = arr[j];
+            }
+        }
+        
+        System.out.println("Largest number of array is :" + largest);
+    }
+}
---
+++ DSA/Array/Twosum.java
@@ -0,0 +1,69 @@
+package Array;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Twosum {
+
+    public static void main (String args[]){
+
+        int[] arr = {0,10 ,20, 20 ,30 ,40 , 0};
+        int destination  = 40 ; 
+
+        // Twosum(arr, destination);
+   
+        List<int[]> res = optimalTwosum(arr, destination);
+        if(res.isEmpty()){
+            System.out.println("Answer not found ");
+        }
+        else{
+            for (int[] pair : res) {
+                System.out.println(pair[0] + " + " + pair[1] + " = " + destination);
+            }
+        }
+    }
+    public  static  void Twosum(int myNum[] ,int target){
+        
+        for(int i=0; i<=myNum.length   ; i++){
+
+            for(int j = i+1 ; j<myNum.length ;j++){
+                if(myNum[i] + myNum[j] == target){
+                    System.out.println(myNum[i]+" + "+myNum[j] +" :"+target);
+                }
+            }
+        }
+    }
+
+    public static List<int[]> optimalTwosum(int[] arr ,int target){
+
+      Map<Integer, Integer> map = new HashMap<>();
+        List<int[]> result = new ArrayList<>();
+        Set<String> seen = new HashSet<>(); // to avoid duplicates
+
+     for(int i = 0 ; i<arr.length;i++){
+
+        int a = arr[i];
+        int more = target - a ;
+        if(map.containsKey(more)){
+
+          String pairKey = Math.min(a, more) + ":" + Math.max(a, more);
+                if (!seen.contains(pairKey)) {
+                    result.add(new int[]{more, a});
+                    seen.add(pairKey);
+                }
+        }else{
+            map.put(arr[i],i);
+        }
+     }
+      return result;
+    }
+
+}
+
