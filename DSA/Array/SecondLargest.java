29482e32cd5df0d5a7277e13ea7a518b1bc5cec7
jay <jkariya524@rku.ac.in>
2025-07-08 11:34:09 +0530

    Slargest

---
+++ DSA/Array/SecondLargest.java
@@ -0,0 +1,27 @@
+package Array;
+
+public class SecondLargest {
+    
+    public static void main (String[] args){
+        int arr[] = {45,78,2,56,7};
+        s_largest(arr);
+    }
+    public static void s_largest(int arr[]){
+
+        int size =arr.length;
+        int largest = arr[0];
+        int SecondLargest = -1;
+
+        for(int i=1 ; i< size   ; i++){
+            if(largest < arr[i] ){
+                
+                SecondLargest = largest;
+                largest = arr[i];
+
+            }else if(arr[i] < largest && arr[i] > SecondLargest ){
+                SecondLargest = arr[i] ; 
+            }
+        }
+        System.out.println(" Second Largest Array Element is " + SecondLargest);
+    }
+}
